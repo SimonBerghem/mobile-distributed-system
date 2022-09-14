c121377509bf9952ec8d9ac1c429f442d910dfbc
HalogTH <HalogTH_socialmedia@protonmail.com>
2022-09-14 10:31:40 +0000

    Revert "c"
    This reverts commit 93f191e22d4df3e7dde068cc9ae00dcfb82dcf44.

--- src/main.go
+++ src/main.go
@@ -1,41 +1,40 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net"
-
-	// "strings"
+    "net"
+	"fmt"
+    // "strings"
 	. "d7024e"
 )
 
-func main() {
+func main (){
 
 	ip := GetOutboundIP()
 	fmt.Println(ip)
 
 	// Create node
-	nodeID := NewRandomKademliaID()
-	me := NewContact(nodeID, ip)
-	routing := NewRoutingTable(me)
-	kademlia := NewKademlia(routing)
+    nodeID := NewRandomKademliaID()
+    me := NewContact(nodeID , ip)
+    routing := NewRoutingTable(me)
+    kademlia := NewKademlia(routing)
 
-	fmt.Println(kademlia)
+    fmt.Println(kademlia)
 
-	// Add node to network
+    // Add node to network
 	// Node listens for requests
 }
 
 // Get preferred outbound ip of this machine
 // Taken from https://stackoverflow.com/questions/23558425/how-do-i-get-the-local-ip-address-in-go
 func GetOutboundIP() string {
-	conn, err := net.Dial("udp", "8.8.8.8:80")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
+    conn, err := net.Dial("udp", "8.8.8.8:80")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer conn.Close()
 
-	localAddr := conn.LocalAddr().(*net.UDPAddr)
+    localAddr := conn.LocalAddr().(*net.UDPAddr)
 
-	return localAddr.IP.String()
+    return localAddr.IP.String()
 }
