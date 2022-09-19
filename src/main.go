9ce820478d49be35a28e5c3f7acf6a7517079514
casperlundberg <cappe5@hotmail.se>
2022-09-19 18:46:37 +0000

    cli started

--- src/main.go
+++ src/main.go
@@ -1,44 +1,46 @@
 package main
 
 import (
-    "log"
-    "net"
-    "fmt"
-    "strconv"
-    "time"
-    // "strings"
+	"fmt"
+	"log"
+	"net"
+	"strconv"
+	"time"
 
-    "github.com/SimonBerghem/mobile-distributed-system/src/d7024e"
+	"github.com/SimonBerghem/mobile-distributed-system/src/d7024e"
+	"github.com/SimonBerghem/mobile-distributed-system/src/kademlia_cli"
 )
 
-func main (){
+func main() {
 
-    defaultIP := "172.20.0.2"
-    port := 4000
-    defaultCon := d7024e.NewContact(d7024e.NewRandomKademliaID(), defaultIP + ":" + strconv.Itoa(port) )
+	kademlia_cli.KademliaCLI()
+
+	defaultIP := "172.20.0.2"
+	port := 4000
+	defaultCon := d7024e.NewContact(d7024e.NewRandomKademliaID(), defaultIP+":"+strconv.Itoa(port))
 
 	ip := GetOutboundIP()
 
 	// Create node
-    nodeID := d7024e.NewRandomKademliaID()
-    me := d7024e.NewContact(nodeID , ip)
-    routing := d7024e.NewRoutingTable(me)
-    node := d7024e.NewKademlia(routing)
-    network := d7024e.NewNetwork(node)
+	nodeID := d7024e.NewRandomKademliaID()
+	me := d7024e.NewContact(nodeID, ip)
+	routing := d7024e.NewRoutingTable(me)
+	node := d7024e.NewKademlia(routing)
+	network := d7024e.NewNetwork(node)
 
-    // fmt.Println(defaultIP != ip, " ", ip)
+	// fmt.Println(defaultIP != ip, " ", ip)
 
-    if defaultIP != ip {    
-        port = 4001
-    } 
-    
-    go d7024e.Listen(ip, port)
-    time.Sleep(5 * time.Second)
-    // fmt.Println(network, defaultCon)
-    fmt.Println("yo")
+	if defaultIP != ip {
+		port = 4001
+	}
 
-    // Add node to network
-    network.SendPingMessage(&defaultCon)
+	go d7024e.Listen(ip, port)
+	time.Sleep(5 * time.Second)
+	// fmt.Println(network, defaultCon)
+	fmt.Println("yo")
+
+	// Add node to network
+	network.SendPingMessage(&defaultCon)
 
 	// Node listens for requests
 }
@@ -46,13 +48,13 @@ func main (){
 // Get preferred outbound ip of this machine
 // Taken from https://stackoverflow.com/questions/23558425/how-do-i-get-the-local-ip-address-in-go
 func GetOutboundIP() string {
-    conn, err := net.Dial("udp", "8.8.8.8:80")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer conn.Close()
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
 
-    localAddr := conn.LocalAddr().(*net.UDPAddr)
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
 
-    return localAddr.IP.String()
+	return localAddr.IP.String()
 }
