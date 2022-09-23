2a5e8226ec3e14117b07d53d50156859ca393aa4
casperlundberg <cappe5@hotmail.se>
2022-09-23 12:35:17 +0000

    kademlia-loop outcommented

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"github.com/SimonBerghem/mobile-distributed-system/d7024e"
+	// "github.com/SimonBerghem/mobile-distributed-system/d7024e"
 	"github.com/SimonBerghem/mobile-distributed-system/kademlia_cli"
 )
 
 func main() {
-	node := d7024e.Kademlia{}
-	node.InitNode()
 	kademlia_cli.InitCLI()
+	// node := d7024e.Kademlia{}
+	// node.InitNode()
 }
