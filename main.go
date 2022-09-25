f02d820cfb9d34f7cf6aa7fdebe6a3642d7b96cb
SimonBerghem <bersim-8@student.ltu.se>
2022-09-25 09:43:57 +0000

    tror jag har nått som kanske funkar för pingande.

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	// "github.com/SimonBerghem/mobile-distributed-system/d7024e"
-	"github.com/SimonBerghem/mobile-distributed-system/kademlia_cli"
+	"github.com/SimonBerghem/mobile-distributed-system/tree/main/kademlia_cli"
 )
 
 func main() {
 	kademlia_cli.InitCLI()
 	// node := d7024e.Kademlia{}
 	// node.InitNode()
+
+	kademlia_cli.PingTest()
 }
