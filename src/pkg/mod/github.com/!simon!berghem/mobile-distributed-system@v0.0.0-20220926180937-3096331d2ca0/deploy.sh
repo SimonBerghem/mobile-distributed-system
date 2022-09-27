cca1451f91a30da768c6a44706a234763bcf4230
SimonBerghem <48282727+SimonBerghem@users.noreply.github.com>
2022-09-27 15:13:44 +0200

    Delete src/pkg/mod directory

--- src/pkg/mod/github.com/!simon!berghem/mobile-distributed-system@v0.0.0-20220926180937-3096331d2ca0/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Run this file from project root folder with sudo sh ./deploy.sh
-#docker stack rm nodes
-docker build . -t kadlab
-# docker stack deploy nodes --compose-file docker-compose.yml
-COMPOSE_HTTP_TIMEOUT=200 docker-compose --compatibility up
-docker ps
