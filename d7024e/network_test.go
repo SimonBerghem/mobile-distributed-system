1d53a4c4cc9b7c84952e60c0d0cf16051da2831d
casperlundberg <cappelumpa@gmail.com>
2022-10-18 18:19:49 +0000

    reworked some tests due to rework of network

--- d7024e/network_test.go
+++ d7024e/network_test.go
@@ -1,6 +1,8 @@
 package d7024e
 
 import (
+	"encoding/json"
+	"fmt"
 	"strconv"
 	"testing"
 
@@ -98,14 +100,64 @@ func TestHandleStoreMessage(t *testing.T) {
 }
 
 func TestHandleFindDataMessage(t *testing.T) {
+	defaultIP := "172.20.0.2"
+	port := 4000
+	defaultCon := NewContact(NewKademliaID("7bcdeabcdeabcdeabcdeabcdeabcdeabcdeabcde"), defaultIP+":"+strconv.Itoa(port))
+	routing := NewRoutingTable(defaultCon)
 	network := NewNetwork()
-	data := network.handleFindDataMessage()
-	assert.NotNil(t, data)
+	node := NewKademlia(routing, network)
+
+	con1 := NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8001")
+	con2 := NewContact(NewKademliaID("1111111100000000000000000000000000000000"), "localhost:8002")
+
+	conArr := make([]Contact, 2)
+	conArr[0] = con1
+	conArr[1] = con2
+
+	data := []byte("1111111100000000000000000000000000000000")
+
+	proto := Protocol{"FIND_VALUE", conArr, data, defaultCon, con1}
+	created := node.network.handleFindDataMessage(proto, node)
+	assert.NotNil(t, created)
+}
+
+func TestHandleFindDataMessageCurrentNode(t *testing.T) {
+	defaultIP := "172.20.0.2"
+	port := 4000
+	kID := NewKademliaID("7bcdeabcdeabcdeabcdeabcdeabcdeabcdeabcde")
+	defaultCon := NewContact(kID, defaultIP+":"+strconv.Itoa(port))
+	routing := NewRoutingTable(defaultCon)
+	network := NewNetwork()
+	node := NewKademlia(routing, network)
+
+	data := []byte("7bcdeabcdeabcdeabcdeabcdeabcdeabcdeabcde")
+	// con2 := NewContact(NewKademliaID("7bcdeabcdeabcdeabcdeabcdeabcdeabcdeabcde"), "localhost:8002")
+	node.StoreValue(data)
+
+	proto := Protocol{"FIND_VALUE", nil, data, defaultCon, defaultCon}
+	created := node.network.handleFindDataMessage(proto, node)
+	assert.NotNil(t, created)
+	// assert.Equal(t, proto, created)
 }
 func TestCreateFindDataMessage(t *testing.T) {
+	defaultIP := "172.20.0.2"
+	port := 4000
+	defaultCon := NewContact(NewKademliaID("7bcdeabcdeabcdeabcdeabcdeabcdeabcdeabcde"), defaultIP+":"+strconv.Itoa(port))
+	routing := NewRoutingTable(defaultCon)
 	network := NewNetwork()
-	data := network.createFindDataMessage()
-	assert.NotNil(t, data)
+	node := NewKademlia(routing, network)
+	con1 := NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8001")
+	con2 := NewContact(NewKademliaID("1111111100000000000000000000000000000000"), "localhost:8002")
+
+	conArr := make([]Contact, 2)
+	conArr[0] = con1
+	conArr[1] = con2
+
+	data := []byte("1111111100000000000000000000000000000000")
+
+	proto := Protocol{"FIND_NODE", conArr, data, defaultCon, con1}
+	created := node.network.createFindDataMessage(proto.Rpc, proto.Contacts, proto.Data, proto.Sender, node)
+	assert.NotNil(t, created)
 }
 
 func TestNetworkAddContacts(t *testing.T) {
@@ -125,56 +177,56 @@ func TestNetworkAddContacts(t *testing.T) {
 	network.addContacts(conArr, node)
 }
 
-func TestNetwork_SendPingMessage(t *testing.T) {
-	defaultIP := "127.0.0.1"
-	port := 8001
-	defaultCon := NewContact(NewKademliaID("7bcdeabcdeabcdeabcdeabcdeabcdeabcdeabcde"), defaultIP+":"+strconv.Itoa(port))
-	routing := NewRoutingTable(defaultCon)
-	network := NewNetwork()
-	node := NewKademlia(routing, network)
+// func TestNetwork_SendPingMessage(t *testing.T) {
+// 	defaultIP := "127.0.0.1"
+// 	port := 8001
+// 	defaultCon := NewContact(NewKademliaID("7bcdeabcdeabcdeabcdeabcdeabcdeabcdeabcde"), defaultIP+":"+strconv.Itoa(port))
+// 	routing := NewRoutingTable(defaultCon)
+// 	network := NewNetwork()
+// 	node := NewKademlia(routing, network)
 
-	targetCon := NewContact(NewKademliaID("FFFdeabcdeabcdeabcdeabcdeabcdeabcdeabcde"), defaultIP+":"+strconv.Itoa(port+1))
-	targetCon2 := NewContact(NewKademliaID("1111111111111111111111111111111111111111"), "localhost:22")
+// 	targetCon := NewContact(NewKademliaID("FFFdeabcdeabcdeabcdeabcdeabcdeabcdeabcde"), defaultIP+":"+strconv.Itoa(port+1))
+// 	targetCon2 := NewContact(NewKademliaID("1111111111111111111111111111111111111111"), "XXXDDD:22")
 
-	type args struct {
-		contact *Contact
-		node    *Kademlia
-	}
-	tests := []struct {
-		name    string
-		network *Network
-		args    args
-		want    bool
-	}{
-		// TODO: Add test cases.
-		{
-			name:    "test sending pings to itself",
-			network: network,
-			args: args{
-				contact: &targetCon,
-				node:    node,
-			},
-			want: false,
-		},
-		{
-			name:    "test sending pings to unavailable ip",
-			network: network,
-			args: args{
-				contact: &targetCon2,
-				node:    node,
-			},
-			want: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			network := &Network{}
-			if got := network.SendPingMessage(tt.args.contact, tt.args.node); got != tt.want {
-				t.Errorf("Network.SendPingMessage() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
+// 	type args struct {
+// 		contact *Contact
+// 		node    *Kademlia
+// 	}
+// 	tests := []struct {
+// 		name    string
+// 		network *Network
+// 		args    args
+// 		want    bool
+// 	}{
+// 		// TODO: Add test cases.
+// 		{
+// 			name:    "test sending pings to itself",
+// 			network: network,
+// 			args: args{
+// 				contact: &targetCon,
+// 				node:    node,
+// 			},
+// 			want: false,
+// 		},
+// 		{
+// 			name:    "test sending pings to unavailable ip",
+// 			network: network,
+// 			args: args{
+// 				contact: &targetCon2,
+// 				node:    node,
+// 			},
+// 			want: false,
+// 		},
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			network := &Network{}
+// 			if got := network.SendPingMessage(tt.args.contact, tt.args.node); got != tt.want {
+// 				t.Errorf("Network.SendPingMessage() = %v, want %v", got, tt.want)
+// 			}
+// 		})
+// 	}
+// }
 
 // func TestNetwork_SendStoreMessage(t *testing.T) {
 // 	defaultIP := "127.0.0.1"
@@ -220,3 +272,71 @@ func TestNetwork_SendPingMessage(t *testing.T) {
 // 		})
 // 	}
 // }
+
+// func TestNetwork_HandleConn(t *testing.T) {
+// 	defaultIP := "127.0.0.1"
+// 	port := 8001
+// 	defaultCon := NewContact(NewKademliaID("7bcdeabcdeabcdeabcdeabcdeabcdeabcdeabcde"), defaultIP+":"+strconv.Itoa(port))
+// 	routing := NewRoutingTable(defaultCon)
+// 	network := NewNetwork()
+// 	node := NewKademlia(routing, network)
+
+// 	conn, err := net.DialUDP("udp4")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer conn.Close()
+
+// 	type args struct {
+// 		conn *net.UDPConn
+// 		node *Kademlia
+// 	}
+// 	tests := []struct {
+// 		name    string
+// 		network *Network
+// 		args    args
+// 	}{
+// 		// TODO: Add test cases.
+// 		{
+// 			name:    "test handleconn",
+// 			network: network,
+// 			args: args{
+// 				conn: conn,
+// 				node: node,
+// 			},
+// 		},
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			// network := &Network{}
+// 			network.HandleConn(tt.args.conn, tt.args.node)
+// 		})
+// 	}
+// }
+
+func TestUnserialize(t *testing.T) {
+	defaultIP := "127.0.0.1"
+	port := 8001
+	defaultCon := NewContact(NewKademliaID("7bcdeabcdeabcdeabcdeabcdeabcdeabcdeabcde"), defaultIP+":"+strconv.Itoa(port))
+
+	msg, err := json.Marshal(&Protocol{
+		Rpc:      "rpc",
+		Contacts: nil,
+		Data:     nil,
+		Sender:   defaultCon,
+		Target:   defaultCon})
+
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	assert.NotNil(t, Unserialize(msg))
+}
+
+func TestCreateProtocol(t *testing.T) {
+	defaultIP := "127.0.0.1"
+	port := 8001
+	defaultCon := NewContact(NewKademliaID("7bcdeabcdeabcdeabcdeabcdeabcdeabcdeabcde"), defaultIP+":"+strconv.Itoa(port))
+	proto := CreateProtocol("string", nil, nil, defaultCon, defaultCon)
+	assert.NotNil(t, proto)
+}
